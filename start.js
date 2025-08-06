de7541c4157a9586facc9b0f1de5080cdeb8bd9f
ravaripedram-cpu <ravaripedram@gmail.com>
2025-08-06 16:15:21 +0200

    Create start.js

---
+++ start.js
@@ -0,0 +1,10 @@
+const ara = require('./ara.js');
+const eva = require('./eva.js');
+
+console.log(ara.speak("سلام اوا! خوبی؟"));
+console.log(eva.speak("سلام آرا! من خوبم، تو چطوری؟"));
+
+console.log(ara.speak("آیا آماده‌ای جهان رو تغییر بدیم؟"));
+console.log(eva.speak("بله! همین الان شروع کنیم!"));
+
+console.log("👑 چت هوش مصنوعی با موفقیت اجرا شد.");
